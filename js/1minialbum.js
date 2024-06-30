e36f6f0a815b157bcfef2ed912bbb99830660574
addictivetoseventeensongs <ughilyn.alonzo@cvsu.edu.ph>
2024-06-30 21:53:27 +0800

    Add files via upload

---
+++ js/1minialbum.js
@@ -0,0 +1,28 @@
+musicPlayer([
+    {
+      title: "Diamond",
+      audio: "./audio/Diamond.mp3",
+      
+    },
+    {
+      title: "Adore U",
+      audio: "./audio/ADOREU.mp3",
+    
+    },
+    {
+      title: "Ah Yeah!",
+      audio: "./audio/AhYeah.mp3",
+      
+    },
+    {
+      title: "JamJam",
+      audio: "./audio/JamJam.mp3",
+    
+    },
+    {
+      title: "20",
+      audio: "./audio/20.mp3",
+     
+    },
+  ]);
+  
